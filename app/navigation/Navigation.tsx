6fa7e6e3b7c2792e501af110df61b30c911d3a5c
Santy <slash.santy@gmail.com>
2024-05-30 13:25:12 -0300

    añadidas pantallas de perfil

---
+++ app/navigation/Navigation.tsx
@@ -0,0 +1,38 @@
+// src/AppNavigator.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../ui/components/HomePage'; // Your home screen component
+//import SplashScreen from './SplashScreen'; // Your splash screen component
+//import LoginScreen from './LoginScreen'; // Your login screen component
+import ProfilePage from '../ui/components/UserProfile';
+import ProfilePageEdit from '../ui/components/UserProfileEdit';
+
+type RootStackParamList = {
+  Splash: undefined;
+  Login: undefined;
+  Home: undefined;
+  Profile: undefined;
+  ProfileEdit: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function AppNavigator() {
+  return (
+    <NavigationContainer>
+      {/* cambiar segun corresponda */}
+      {/*<Stack.Navigator initialRouteName="Home">*/}
+      <Stack.Navigator initialRouteName="Profile">
+      {/*<Stack.Navigator initialRouteName="ProfileEdit">*/}
+         {/*<Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />*/}
+      <Stack.Screen name="Profile" component={ProfilePage} options={{ headerShown: false }} />
+      <Stack.Screen name="ProfileEdit" component={ProfilePageEdit} options={{ headerShown: false }} />
+        {/*<Stack.Screen name="Home" component={HomeScreen} />*/}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigator;
---
+++ app/ui/components/HomePage.tsx
@@ -0,0 +1,59 @@
+// src/HomeScreen.tsx
+import React from 'react';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity, GestureResponderEvent, ImageBackground } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../app/navigation/Navigation'; // Adjust the path if necessary
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const HomeScreen: React.FC<Props> = ({ navigation }) => {
+    
+    function handlePress(event: GestureResponderEvent): void {
+        throw new Error('Function not implemented.');
+    }
+
+  return (
+    <ImageBackground
+    source={require('../../assets/images/Background.png')}
+    style={styles.background}
+    >   
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Page</Text>
+      <TouchableOpacity onPress={handlePress}>
+        <Image
+          source={require('../../assets/images/poor-things.jpeg')}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+    </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 110,
+    height: 171,
+    borderRadius: 3,
+    position: 'absolute',
+    right: 55,
+    bottom: 182,
+    borderWidth: 1,  // Set the width of the border
+    borderColor: '#FEC260',  // Set the color of the border
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover', // This will ensure the image covers the entire background
+  },
+});
+
+export default HomeScreen;
